bb04a9bcb88132861021411a8cff85868cf75d22
jose-alves-fn <josealvesfn@gmail.com>
2024-08-05 11:19:48 -0300

    atualizando projeto

---
+++ src/main/java/br/com/alura/consultando_tabela_fipe/ConsultandoTabelaFipeApplication.java
@@ -0,0 +1,13 @@
+package br.com.alura.consultando_tabela_fipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsultandoTabelaFipeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsultandoTabelaFipeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/alura/consultando_tabela_fipe/ConsultandoTabelaFipeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.alura.consultando_tabela_fipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsultandoTabelaFipeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
